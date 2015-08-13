73c70c8a00eb3fd212734388c2bf31e19f4cab0a
Evan Schneider <evanrs@gmail.com>
2015-08-13 15:24:27 -0400

    Adds solution.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var http = require('http');
+var EventEmitter = require('events').EventEmitter;
 
 var app = require('express')();
 var users = [];
+var emitter = new EventEmitter();
+
+emitter.on('user add', function (user) {
+    users.push(user);
+
+    emitter.emit('users updated', users);
+});
 
 app.get('/api/users/add/:user', function (request, response) {
-    users.push(request.params.user);
-
-    response.send(users);
+    emitter.once('users updated', response.send.bind(response));
+    emitter.emit('user add', request.params.user);
 });
 
 app.listen(3000, function () {
