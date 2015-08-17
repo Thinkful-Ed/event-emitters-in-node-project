0db1517ee1a71b8e5af000ace030d55eeefa022f
Evan Schneider <evanrs@gmail.com>
2015-08-17 16:27:15 -0400

    Add endpoint to store image urls.

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var http = require('http');
 
-var server = require('express')();
+var app = require('express')();
 
 var db = require('./db');
 
-server.get('/', function (request, response) {
+app.get('/', function (request, response) {
     db.all(function (err, data) {
         response.send(data);
     });
 });
 
-server.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+app.get('/image/new', function (request, response) {
+    db.insert(request.query.imageUrl, function (err, data) {
+        response.send(data);
+    });
+});
 
-    console.log('Listening at http://%s:%s', host, port);
+var server = app.listen(3000, function () {
+    console.log('Listening at', server.address().port);
 });
