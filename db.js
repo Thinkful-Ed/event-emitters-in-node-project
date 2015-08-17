c3c4725877033c6059126237e9f1084cef976565
Evan Schneider <evanrs@gmail.com>
2015-08-17 14:36:34 -0400

    Adds mock db.

---
+++ db.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+
+var FILENAME = './db.json';
+
+function read (callback) {
+    fs.readFile(FILENAME, function (err, database) {
+        if (err)
+            console.warn("Unexpected error reading database:\n", err.message);
+
+        database = database ? JSON.parse(database) : [];
+
+        callback && callback(null, database);
+    });
+}
+
+function write (database, callback) {
+    fs.writeFile(FILENAME, JSON.stringify(database, null, 4), function (err) {
+        if (err)
+            console.warn("Unexpected error writing database:\n", err.message);
+
+        callback && callback(null, database);
+    });
+}
+
+read(function (err, database) {
+    write(database);
+});
+
+module.exports = {
+    all: function all (callback) {
+        read(callback);
+    },
+
+    insert: function insert (url, callback) {
+        read(function (err, data) {
+            data.push({url: url, createdAt: new Date()});
+
+            write(data, callback);
+        });
+    }
+}
