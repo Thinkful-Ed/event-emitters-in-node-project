5b25f5d8e8999577e0343b696ea6d29ababd6921
Evan Schneider <evanrs@gmail.com>
2015-08-18 10:23:49 -0400

    Expand solution

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
-var http = require('http');
+var EventEmitter = require('events').EventEmitter;
+var fs = require('fs');
+// npm
+var request = require('request');
+var md5 = require('md5');
+
 
 var app = require('express')();
-
 var db = require('./db');
+var publisher = new EventEmitter();
+
+var emit = publisher.emit;
+publisher.emit = function () {
+    console.log(arguments[0]);
+    return emit.apply(publisher, arguments);
+};
+
+publisher.on('image:new', function (imageUrl) {
+    db.upsert(imageUrl, function (err, data) {
+        publisher.emit('image:stored');
+    });
+});
+
+publisher.on('image:new', function (imageUrl) {
+    // Archive image
+    request({url: imageUrl, encoding: 'binary'}, function (err, response, body) {
+        if (err) {
+            publisher.emit('image:archive:error', imageUrl, err);
+            return;
+        }
+
+        var path = './static/' + md5(imageUrl) + '.' + imageUrl.split('.').pop();
+
+        fs.writeFile(path, body, 'binary', function (err) {
+            if (err) {
+                publisher.emit('image:archive:error', imageUrl, err);
+                return;
+            }
+            db.upsert(imageUrl, {path: path}, function (err, data) {
+                // Emit archive success event
+                publisher.emit('image:archived', imageUrl, data);
+            });
+        })
+
+    })
+});
+
+publisher.on('image:archive:error', function (imageUrl, err) {
+    console.warn("Error archiving image", imageUrl, err)
+})
 
 app.get('/', function (request, response) {
     db.all(function (err, data) {
@@ -11,9 +56,11 @@ app.get('/', function (request, response) {
 });
 
 app.get('/image/new', function (request, response) {
-    db.insert(request.query.imageUrl, function (err, data) {
-        response.send(data);
+    publisher.once('image:stored', function () {
+        response.sendStatus(200);
     });
+
+    publisher.emit('image:new', request.query.imageUrl);
 });
 
 var server = app.listen(3000, function () {
